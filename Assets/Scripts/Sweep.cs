03ab2ed57e9b596dfebdb5ce2705ec253ae0748b
TWMangrove <80806564+TWMangrove@users.noreply.github.com>
2022-08-29 21:38:43 -0500

    Completed

--- Assets/Scripts/Sweep.cs
+++ Assets/Scripts/Sweep.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Sweep : MonoBehaviour
 {
     // Update is called once per frame
+    [SerializeField] float sweepSpd = 1f;
     void Update()
     {
         if (Time.time % 6 < 3)
         {
-            transform.Translate(0,0,0.01f);
+            transform.Translate(0,0,0.01f*sweepSpd);
         }
         if (Time.time % 6 >= 3)
         {
-            transform.Translate(0,0,-0.01f);
+            transform.Translate(0,0,-0.01f*sweepSpd);
         }
     }
 }
